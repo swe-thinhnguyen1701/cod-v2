cb42f6be92b262c03bd451864041a704d4a08b01
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-08-18 21:25:54 -0700

    Update theme to make responsive heading

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -8,6 +8,21 @@ const config: ThemeConfig = {
 
 const theme = extendTheme({
   config,
+  components: {
+    Heading: {
+      sizes: {
+        "h1": {
+          fontSize: {base: "2.441rem", lg: "3.052rem", xl: "3.815rem"}
+        },
+        "h2": {
+          fontSize: {base: "1.953rem", lg: "2.441rem", xl: "3.052rem"}
+        },
+        "h3": {
+          fontSize: {base: "1rem", lg: "1.3rem"}
+        }
+      }
+    }
+  }
 });
 
 export default theme;

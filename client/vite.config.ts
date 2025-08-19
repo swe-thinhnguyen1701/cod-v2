b0683fc9cf90c11d3fb7c824d939e99ef6244414
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-08-18 21:25:16 -0700

    Fix typo error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      "/grapql": {
+      "/graphql": {
         target: "http://localhost:3001",
         secure: false,
         changeOrigin: true,

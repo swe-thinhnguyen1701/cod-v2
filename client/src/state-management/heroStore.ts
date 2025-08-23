6d39d02b92b0aae52fb107ececd720906012693b
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-08-23 09:26:28 -0700

    Build hero store state management

---
+++ client/src/state-management/heroStore.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import type HeroEntity from "../entities/HeroEntity";
+
+interface HeroStore {
+  hero: HeroEntity | null;
+  setHero: (hero: HeroEntity) => void;
+}
+
+const useHeroStore = create<HeroStore>((set) => ({
+  hero: null,
+  setHero: (hero: HeroEntity) => set({ hero })
+}));
+
+export default useHeroStore;

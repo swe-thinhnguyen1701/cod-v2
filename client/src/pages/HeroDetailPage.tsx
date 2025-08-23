c6780e601e5142a0be7e8212b31f24ae4e8456ae
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-08-23 09:40:00 -0700

    Build HeroDetail page

---
+++ client/src/pages/HeroDetailPage.tsx
@@ -0,0 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_HERO_DETAIL_BY_NAME } from "../graphql/queries";
+import { useEffect } from "react";
+import { Heading, VStack } from "@chakra-ui/react";
+import useHeroStore from "../state-management/heroStore";
+import Spinner from "../components/Spinner";
+import HeroInfo from "../components/HeroInfo";
+
+
+const HeroDetailPage = () => {
+    const { heroName: heroName } = useParams();
+    const { loading, error, data } = useQuery(GET_HERO_DETAIL_BY_NAME, {
+        variables: { heroName: heroName }
+    });
+    const { setHero } = useHeroStore();
+
+    useEffect(() => {
+        if(data?.getHeroDetailByName)
+            setHero(data.getHeroDetailByName);
+    }, [data, setHero]);
+
+    if (loading)
+        return <Spinner />
+
+    if (error)
+        return <Heading>{`Something went wrong ${error.message}`}</Heading>
+
+    return (
+        <VStack>
+            <HeroInfo />
+        </VStack>
+    )
+}
+
+export default HeroDetailPage;

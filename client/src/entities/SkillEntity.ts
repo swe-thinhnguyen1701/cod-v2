933e24348ca0f585c3d0fe8a56c6750493d31732
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-01 12:21:07 -0700

    feat: Update Skill entity to match new model

--- client/src/entities/SkillEntity.ts
+++ client/src/entities/SkillEntity.ts
@@ -1,6 +1,6 @@
 export default interface SkillEntity {
     name: string,
-    description: string,
+    description: [string],
     image: string,
     previews: [string],
     rage_cost: number | null

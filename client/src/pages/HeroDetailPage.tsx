e57b38e9f6e08c6d3d2a04d88721894cb32b640b
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-01 12:18:51 -0700

    feat: Update HeroDetail page for dynamic rendering

--- client/src/pages/HeroDetailPage.tsx
+++ client/src/pages/HeroDetailPage.tsx
@@ -7,10 +7,12 @@ import useHeroStore from "../state-management/heroStore";
 import Spinner from "../components/Spinner";
 import HeroInfo from "../components/HeroInfo";
 import HeroSkill from "../components/HeroSkill";
+import RecommendationList from "../components/RecommendationList";
 
+const RECOMMENDATION_LIST = ["artifact", "pet", "partner"] as const;
 
 const HeroDetailPage = () => {
-    const {colorMode} = useColorMode();
+    const { colorMode } = useColorMode();
     const { heroName: heroName } = useParams();
     const { loading, error, data } = useQuery(GET_HERO_DETAIL_BY_NAME, {
         variables: { heroName: heroName }
@@ -31,11 +33,31 @@ const HeroDetailPage = () => {
     return (
         <VStack gap="100px">
             <HeroInfo />
-            <Card rounded="md" padding="10px 0" backgroundColor={colorMode === "light" ? "blackAlpha.50" : "gray.700"}>
-                <HeroSkill />
-            </Card>
-            <Flex>
-                
+            <Flex flexDirection={{ base: "column", xl: "row" }} gap="50px">
+                <Card
+                    rounded="md"
+                    padding={{ base: "10px 0", md: "10px 20px" }}
+                    backgroundColor={colorMode === "light" ? "blackAlpha.50" : "gray.700"}>
+                    <HeroSkill />
+                </Card>
+                <Flex 
+                flexDirection={{base: "column", md:"row",  xl: "column"}} 
+                gap="50px"
+                flexWrap="wrap"
+                >
+                    {RECOMMENDATION_LIST.map((type, idx) => (
+                        <Card
+                        rounded="md"
+                        padding={{ base: "10px 0", md: "10px 20px" }}
+                        backgroundColor={colorMode === "light" ? "blackAlpha.50" : "gray.700"}
+                        width={{ base: "100vw", md: "360px" }}
+                        key={idx}
+                    >
+                        <RecommendationList type={type} />
+                    </Card>
+                    ))}
+                </Flex>
+
             </Flex>
         </VStack>
     )

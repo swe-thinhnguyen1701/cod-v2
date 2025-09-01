bd97981cba4cc6c8f48d1d11cd97a97c7fbe14c0
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-01 12:20:25 -0700

    feat: Update RecommendationList component to be more dynamic and clean.

--- client/src/components/RecommendationList.tsx
+++ client/src/components/RecommendationList.tsx
@@ -1,32 +1,73 @@
-import { Image, ListItem, UnorderedList } from "@chakra-ui/react";
+import { Box, Image, ListItem, Tooltip, UnorderedList } from "@chakra-ui/react";
+import SectionHeading from "./SectionHeading";
+import useHeroStore from "../state-management/heroStore";
+import Spinner from "./Spinner";
+import type HeroEntity from "../entities/HeroEntity";
 
 interface Props {
-    data: string[],
-    type: "hero" | "pet" | "artifact"
+    type: "partner" | "pet" | "artifact"
 }
 
-const IMG_URLS = [
-    "https://d3bhl6gkk81cq1.cloudfront.net/hero-avatar/",
-    "https://d3bhl6gkk81cq1.cloudfront.net/pets/",
-    "https://d3bhl6gkk81cq1.cloudfront.net/artifacts/"
-]
+interface Configs {
+    imgUrl: string;
+    title: string;
+    getData: (hero: HeroEntity) => string[] | undefined;
+}
 
-const RecommendationList = ({ data, type }: Props) => {
-    const imgUrl = type === "hero" ? IMG_URLS[0] : type === "pet" ? IMG_URLS[1] : IMG_URLS[2];
+const CONFIGS: Record<Props["type"], Configs> = {
+    partner: {
+        imgUrl: "https://d3bhl6gkk81cq1.cloudfront.net/hero-avatar/",
+        title: "Partners",
+        getData: (hero) => hero?.partners || undefined
+    },
+    pet: {
+        imgUrl: "https://d3bhl6gkk81cq1.cloudfront.net/pets/",
+        title: "Pets",
+        getData: (hero) => hero?.pets || undefined
+    },
+    artifact: {
+        imgUrl: "https://d3bhl6gkk81cq1.cloudfront.net/artifacts/",
+        title: "Artifacts",
+        getData: (hero) => hero?.artifacts || undefined
+    }
+};
+
+const RecommendationList = ({ type }: Props) => {
+    const { hero } = useHeroStore();
+    const { imgUrl, title, getData } = CONFIGS[type];
+    const data = getData(hero!);
+
+    console.log(data)
+
+    if (!data) return <Spinner />;
+
     return (
-        <UnorderedList
-            display="flex"
-            flexDirection="row"
-            flexWrap="wrap"
-            gap={4}
-            listStyleType="none"
-            margin={0}>
+        <Box>
+            <SectionHeading title={title} smallSize={true} />
+            <UnorderedList
+                display="flex"
+                flexDirection="row"
+                flexWrap="wrap"
+                gap={4}
+                listStyleType="none"
+                margin={0}>
                 {data.map((item, idx) => (
-                    <ListItem key={idx}>
-                        <Image src={`${imgUrl}${item}.webp`} alt={`${item} image`}/>
+                    <ListItem
+                        key={idx}
+                        className="recommended-item">
+                        <Tooltip label={item} hasArrow closeOnClick={false}>
+                            <Box width={{ base: "50px" }} >
+                                <Image
+                                    src={`${imgUrl}${item}.webp`}
+                                    alt={`${item} image`}
+                                    width="100%"
+                                />
+                            </Box>
+                        </Tooltip>
                     </ListItem>
                 ))}
-        </UnorderedList>
+            </UnorderedList>
+        </Box>
     )
 }
 

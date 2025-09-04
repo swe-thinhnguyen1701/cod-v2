49f5d18c11c4cdcde9fdf29f907730e7be67650b
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-03 17:47:54 -0700

    feat: Implement BlurImage component in ItemCard

--- client/src/components/ItemCard.tsx
+++ client/src/components/ItemCard.tsx
@@ -1,6 +1,8 @@
-import { Card, CardBody, CardFooter, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, Heading } from "@chakra-ui/react";
 import type HeroBriefEntity from "../entities/HeroEntity";
 import RoleBadgeList from "./RoleBadgeList";
+// import { useState } from "react";
+import BlurImage from "./BlurImage";
 
 interface Props {
     data: HeroBriefEntity
@@ -9,6 +11,8 @@ interface Props {
 const HERO_IMG_URL = "https://d3bhl6gkk81cq1.cloudfront.net/hero-full/"
 
 const ItemCard = ({ data }: Props) => {
+    // const [loaded, setLoaded] = useState(false);
+
     return (
         <Card width="300px" height="370px" overflow="hidden" className="item-card">
             <CardBody
@@ -18,8 +22,17 @@ const ItemCard = ({ data }: Props) => {
                 alignItems="center"
                 pb={2}
                 className={`item-card-body__${data.rarity === 1 ? "legend" : data.rarity === 2 ? "epic" : "common"}`}>
-                <Image className="item-image" src={`${HERO_IMG_URL}${data.name}.webp`} alt={`${data.name} image`} />
-                <RoleBadgeList roles={data.roles} gap={4} isShorten/>
+                {/* <Image
+                    alt={`${data.name} image`}
+                    className="item-image"
+                    loading="lazy"
+                    src={`${HERO_IMG_URL}${data.name}.webp`}
+                    transition="filter 1s ease-out"
+                    filter={loaded ? "blur(0px)" : "blur(20px)"}
+                    onLoad={() => setLoaded(true)}
+                /> */}
+                <BlurImage src={`${HERO_IMG_URL}${data.name}.webp`} alt={`${data.name} image`} />
+                <RoleBadgeList roles={data.roles} gap={4} isShorten />
             </CardBody>
             <CardFooter display={"flex"} justifyContent={"center"} bg="gray.900" color="white" fontWeight="bold" padding={0} className="item-card-footer">
                 <Heading as="h3" size="h3" transition={"font-size 0.3s ease-in-out"}>

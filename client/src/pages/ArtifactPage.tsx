6c7d30a9cb3197e4b89a9cc56a33d5b13647408b
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-07 13:37:28 -0700

    feat: build artifact page

---
+++ client/src/pages/ArtifactPage.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from "@apollo/client";
+import { GET_ALL_ARTIFACTS } from "../graphql/queries";
+import { Heading, UnorderedList, ListItem, Text, VStack } from "@chakra-ui/react";
+import type HeroBriefEntity from "../entities/HeroEntity";
+import ItemCard from "../components/ItemCard";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+
+const ArtifactPage = () => {
+    const { data, error, loading } = useQuery(GET_ALL_ARTIFACTS);
+
+    if (loading){
+        return (
+         <VStack minHeight="90vh" justifyContent="center">
+            <Spinner />
+         </VStack>   
+        )
+    }
+
+    if (error) {
+        console.log(error);
+        return <Heading>Something went wrong {error.message}</Heading>
+    }
+
+    const artifacts: HeroBriefEntity[] = data.getAllArtifacts;
+
+    return (
+        <VStack>
+            <VStack mb={8}>
+                <Heading as="h1" size="h1">Artifacts</Heading>
+                <Text textAlign="center">Artifacts are powerful items that boost your heroes and shape your battle strategy. Explore the full list below and click on any artifact to see its skills, upgrades, and how to use it effectively.</Text>
+            </VStack>
+            <UnorderedList display="flex" flexWrap="wrap" justifyContent="center" gap={8} listStyleType="none" padding={0} margin={0}>
+                {artifacts.map(artifact => (
+                    <ListItem key={artifact.id} cursor="pointer">
+                        <Link to={`/artifacts/${artifact.name}`}>
+                            <ItemCard data={artifact} key={artifact.id} />
+                        </Link>
+                    </ListItem>
+                ))}
+            </UnorderedList>
+        </VStack>
+    )
+}
+
+export default ArtifactPage;

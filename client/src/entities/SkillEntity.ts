cbca92cad55366158cd06640c83170656fb54b7c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-07 13:37:10 -0700

    refactor(artifact): add properties to align with skill attributes

--- client/src/entities/SkillEntity.ts
+++ client/src/entities/SkillEntity.ts
@@ -4,4 +4,7 @@ export default interface SkillEntity {
     image: string,
     previews: [string],
     rage_cost: number | null
+    additonal_effect: string | null
+    exemplar_effect: string | null
+    cooldown: string | null
 }

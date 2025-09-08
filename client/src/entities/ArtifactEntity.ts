a2ccb45984b4b220c79d69bfdf5747c373db969f
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-08 00:17:00 -0700

    refactor(artifact): correct datatype of values property in ArtifactStat interface

--- client/src/entities/ArtifactEntity.ts
+++ client/src/entities/ArtifactEntity.ts
@@ -1,11 +1,18 @@
 import type RoleEntity from "./RoleEntity";
 import type SkillEntity from "./SkillEntity";
 
+interface ArtifactStat {
+  name: string;
+  values: number[];
+  image: string;
+}
+
 export default interface ArtifactEntity {
   name: string;
   rarity: number;
   roles: [RoleEntity];
   skills: [SkillEntity];
+  stats: [ArtifactStat];
   previews: [string];
   cooldown: string | null;
   image: string;

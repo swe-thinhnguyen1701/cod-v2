be04b0ba748fd1e5bad7774a0ab51703c796b074
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-08 22:51:55 -0700

    feat(store): extend SkillStore to support hero, artifact, and pet skills

--- client/src/state-management/skillStore.ts
+++ client/src/state-management/skillStore.ts
@@ -2,13 +2,41 @@ import { create } from "zustand";
 import type SkillEntity from "../entities/SkillEntity";
 
 interface SkillStore {
-    selectedSkill: SkillEntity | null;
-    setSelectedSkill: (skill: SkillEntity) => void;
+  selectedSkill: SkillEntity | null;
+  isHeroSkill: boolean;
+  isArtifactSkill: boolean;
+  isPetSkill: boolean;
+  setHeroSkill: (skill: SkillEntity) => void;
+  setArtifactSkill: (skill: SkillEntity) => void;
+  setPetSkill: (skill: SkillEntity) => void;
 }
 
 const useSkillStore = create<SkillStore>((set) => ({
-    selectedSkill: null,
-    setSelectedSkill: (skill: SkillEntity) => set({ selectedSkill: skill })
+  selectedSkill: null,
+  isHeroSkill: false,
+  isArtifactSkill: false,
+  isPetSkill: false,
+  setHeroSkill: (skill: SkillEntity) =>
+    set({
+      selectedSkill: skill,
+      isHeroSkill: true,
+      isArtifactSkill: false,
+      isPetSkill: false,
+    }),
+  setArtifactSkill: (skill: SkillEntity) =>
+    set({
+      selectedSkill: skill,
+      isHeroSkill: false,
+      isArtifactSkill: true,
+      isPetSkill: false,
+    }),
+  setPetSkill: (skill: SkillEntity) =>
+    set({
+      selectedSkill: skill,
+      isHeroSkill: false,
+      isArtifactSkill: false,
+      isPetSkill: true,
+    }),
 }));
 
-export default useSkillStore
+export default useSkillStore;

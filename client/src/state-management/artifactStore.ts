d4eb0c58af30aa6068725b25ba7f97b6985b9617
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-08 22:53:53 -0700

    feat(artifact): extend store to manage selectedArtifactRank

--- client/src/state-management/artifactStore.ts
+++ client/src/state-management/artifactStore.ts
@@ -3,12 +3,16 @@ import type ArtifactEntity from "../entities/ArtifactEntity";
 
 interface ArtifactStore {
   artifact: ArtifactEntity | null;
+  selectedArtifactRank: string;
   setArtifacts: (artifacts: ArtifactEntity) => void;
+  setArtifactRank: (rank: string) => void;
 }
 
 const useArtifactStore = create<ArtifactStore>((set) => ({
   artifact: null,
+  selectedArtifactRank: "Legendary",
   setArtifacts: (artifact: ArtifactEntity) => set({ artifact }),
+  setArtifactRank: (rank: string) => set({ selectedArtifactRank: rank })
 }));
 
 export default useArtifactStore;

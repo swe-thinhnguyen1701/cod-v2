6544d7d514fdcba9a0f6be55990c397856688b99
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-11 11:13:24 -0700

    feat(pet): build Pet model

---
+++ server/models/Pet.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const petSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    attributes: [{
+        type: Number,
+        required: true
+    }],
+    roles: [{
+        type: Schema.Types.ObjectId,
+        ref: "Role"
+    }],
+    recommended_skills: [[
+        {
+            // type: Schema.Types.ObjectId,
+            // ref: "Skill"
+            type: String,
+        }
+    ]],
+    recommended_heroes: [{
+        type: String
+    }],
+    is_flying: {
+        type: Boolean,
+    },
+    image: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+const Pet = model("Pet", petSchema);
+
+module.exports = Pet;

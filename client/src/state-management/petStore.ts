454d0d858041f12ab4efb1b00839787b04e9309b
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-11 14:47:55 -0700

    refactor(pet-store): add new method to reset selected pet

--- client/src/state-management/petStore.ts
+++ client/src/state-management/petStore.ts
@@ -4,11 +4,13 @@ import type PetEntity from "../entities/PetEntity";
 interface PetStore {
     pet: PetEntity | null;
     setPet: (pet: PetEntity) => void;
+    reset: () => void;
 }
 
 const usePetStore = create<PetStore>((set) => ({
     pet: null,
-    setPet: (pet: PetEntity) => set({ pet })
+    setPet: (pet: PetEntity) => set({ pet }),
+    reset: () => set({ pet: null })
 }));
 
 export default usePetStore;

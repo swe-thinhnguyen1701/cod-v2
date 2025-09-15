8a68616720c89649a32f57912c2b8af2a2b4ebe6
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-14 19:31:49 -0700

    refactor(seed-skills): separate hero, artifact, and pet skill seeding

--- server/seeds/seed-skills.js
+++ server/seeds/seed-skills.js
@@ -1,13 +1,20 @@
 const db = require("../config/db-connection");
-const { Skill } = require("../models");
-const skills = require("../database/skills.json");
+const { Skill, HeroSkill, ArtifactSkill, PetSkill } = require("../models");
+const heroSkills = require("../database/skill-hero.json");
+const artifactSkills = require("../database/skill-artifact.json");
+const petSkills = require("../database/skill-pet.json");
 
 const seedSkills = async () => {
     db.once("open", async () => {
         try {
             console.log("Seeding SKILLS...");
             await Skill.deleteMany({});
-            await Skill.insertMany(skills);
+            await HeroSkill.insertMany(heroSkills);
+            console.log("HERO SKILLS inserted SUCCESSFULLY ☘️");
+            await ArtifactSkill.insertMany(artifactSkills);
+            console.log("ARTIFACT SKILLS inserted SUCCESSFULLY ☘️");
+            await PetSkill.insertMany(petSkills);
+            console.log("PET SKILLS inserted SUCCESSFULLY ☘️");
         } catch (error) {
             console.error("Error seeding skills ❌:", error);
             process.exit(1);

346bd920130f54e1125606ad299b121b45087a23
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-14 19:40:29 -0700

    refactor(resolvers): update getPetDetailByName to populate recommended skills

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,45 +1,50 @@
-const { get } = require('mongoose');
-const {Hero, Artifact, Pet} = require('../models');
+const { Hero, Artifact, Pet } = require('../models');
 
 const resolvers = {
     Query: {
         getAllHeroes: async () => {
-            const heroes = await Hero.find().populate("roles").sort({name: 1});
+            const heroes = await Hero.find().populate("roles").sort({ name: 1 });
             if (!heroes || heroes.length === 0) {
                 throw new Error('No heroes found');
             }
             return heroes;
         },
-        getHeroDetailByName: async (_parent, {heroName}) => {
-            const hero = await Hero.findOne({name: heroName}).populate(["roles", "skills", "faction"]);
+        getHeroDetailByName: async (_parent, { heroName }) => {
+            const hero = await Hero.findOne({ name: heroName }).populate(["roles", "skills", "faction"]);
             if (!hero) {
                 throw new Error('Hero not found');
             }
             return hero;
         },
         getAllArtifacts: async () => {
-            const artifacts = await Artifact.find().populate("roles").sort({name: 1});
+            const artifacts = await Artifact.find().populate("roles").sort({ name: 1 });
             if (!artifacts || artifacts.length === 0) {
                 throw new Error('No artifacts found');
             }
             return artifacts;
         },
-        getArtifactDetailByName: async (_parent, {artifactName}) => {
-            const artifact = await Artifact.findOne({name: artifactName}).populate(["roles", "skills"]);
+        getArtifactDetailByName: async (_parent, { artifactName }) => {
+            const artifact = await Artifact.findOne({ name: artifactName }).populate(["roles", "skills"]);
             if (!artifact) {
                 throw new Error('Artifact not found');
             }
             return artifact;
         },
         getAllPets: async () => {
-            const pets = await Pet.find().populate("roles").sort({name: 1});
+            const pets = await Pet.find().populate("roles").sort({ name: 1 });
             if (!pets || pets.length === 0) {
                 throw new Error('No pets found');
             }
             return pets;
         },
-        getPetDetailByName: async (_parent, {petName}) => {
-            const pet = await Pet.findOne({name: petName}).populate(["roles"]);
+        getPetDetailByName: async (_parent, { petName }) => {
+            const pet = await Pet
+                .findOne({ name: petName })
+                .populate("roles")
+                .populate({
+                    path: "recommended_skills",
+                    model: "Skill"
+                });
             if (!pet) {
                 throw new Error('Pet not found');
             }

6f82faee94f9112675c8a2adb26de7ca7439483a
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-14 19:41:01 -0700

    refactor(Skill): modify Skill model

--- server/models/Skill.js
+++ server/models/Skill.js
@@ -1,48 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const skillSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
+const options = { discriminatorKey: "skillType", collection: "skills" };
+
+const baseSkillSchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        description: [{ type: String, required: true }],
+        owner: { type: String },
     },
-    description: [
-        {
-            type: String,
-            required: true
-        }
-    ],
-    previews: [
-        {
-            type: String
-        }
-    ],
-    image: {
-        type: String,
-        required: false
-    },
-    rage_cost: {
-        type: Number,
-        required: false
-    },
-    additional_effect: {
-        type: String,
-        required: false
-    },
-    exemplar_effect: {
-        type: String,
-        required: false
-    },
-    cooldown: {
-        type: String,
-        required: false
-    },
-    owner: {
-        type: String,
-        required: false,
-    }
+    options
+);
+
+const Skill = model("Skill", baseSkillSchema);
+
+// Hero-specific
+const heroSkillSchema = new Schema({
+    rage_cost: { type: Number },
+    previews: [{ type: String }],
+    image: { type: String },
 });
 
-const Skill = model('Skill', skillSchema);
+// Artifact-specific
+const artifactSkillSchema = new Schema({
+    rage_cost: { type: Number },
+    cooldown: { type: String },
+    additional_effect: { type: String },
+    exemplar_effect: { type: String },
+    previews: [{ type: String }]
+});
 
-module.exports = Skill;
+// Pet-specific
+const petSkillSchema = new Schema({
+    scaling_values: [[{ type: Number }]], // e.g. [[10, 15, 20]]
+    attribute: { type: String }, // e.g. "strength", "agility", "intelligence"
+    image: { type: String },
+    is_rage: { type: Boolean },
+    is_talent: { type: Boolean }
+});
+
+const ArtifactSkill = Skill.discriminator("ArtifactSkill", artifactSkillSchema);
+const HeroSkill = Skill.discriminator("HeroSkill", heroSkillSchema);
+const PetSkill = Skill.discriminator("PetSkill", petSkillSchema);
+
+module.exports = { Skill, ArtifactSkill, HeroSkill, PetSkill };

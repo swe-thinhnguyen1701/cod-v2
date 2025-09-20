1bd58e2a8c70c995118fada0c6f12313433f8071
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-20 13:57:24 -0700

    feat(pet-detail): invoke pet attribute initializer and improve layout responsiveness

--- client/src/pages/PetDetailPage.tsx
+++ client/src/pages/PetDetailPage.tsx
@@ -1,25 +1,28 @@
-import { Heading, Flex, Card } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import usePetStore from "../state-management/petStore";
 import { GET_PET_DETAIL_BY_NAME } from "../graphql/queries";
 import { useQuery } from "@apollo/client";
-// import { useEffect } from "react";
+import { Heading, Flex, Card } from "@chakra-ui/react";
+import usePetStore from "../state-management/petStore";
 import Spinner from "../components/Spinner";
 import PetInfo from "../components/PetInfo";
-import { useEffect } from "react";
 import PetSkill from "../components/PetSkill";
+import type { PetEntity } from "../entities/PetEntity";
 
 const PetDetailPage = () => {
     const { petName: petName } = useParams();
-    const { setPet } = usePetStore();
+    const { setPet, initializePetAttributes } = usePetStore();
     const { loading, error, data } = useQuery(GET_PET_DETAIL_BY_NAME, ({
         variables: { petName: petName }
     }));
 
     useEffect(() => {
-        if (data)
-            setPet(data.getPetDetailByName);
-    }, [data, setPet])
+        if (data) {
+            const pet: PetEntity = data.getPetDetailByName
+            setPet(pet);
+            initializePetAttributes(pet.attributes);
+        }
+    }, [data, setPet, initializePetAttributes])
 
     if (loading)
         return <Spinner />
@@ -27,13 +30,12 @@ const PetDetailPage = () => {
     if (error)
         return <Heading>{`${error.message}`}</Heading>
 
-
     return (
-        <Flex width="100%" flexDir={{base: "column"}}>
-            <Card py={4} width={{base: "100%", lg: "550px"}}>
+        <Flex width="100%" flexDir={{ base: "column", lg: "row" }} gap={8}>
+            <Card py={4} width={{ base: "100%", lg: "550px" }}>
                 <PetInfo />
             </Card>
-            <Card width={{base: "100%", lg: "700px"}}>
+            <Card width={{ base: "100%", lg: "700px" }}>
                 <PetSkill />
             </Card>
         </Flex>

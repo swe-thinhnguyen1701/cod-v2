1ae9da8982ee0cfeb93640daf069796e9f76b1a9
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-20 13:27:10 -0700

    refactor(Pet-Skill): Implement NumOfPetSkills and NumOfSkillSets component and clean up code.

--- client/src/components/PetSkill.tsx
+++ client/src/components/PetSkill.tsx
@@ -1,43 +1,17 @@
-import { Box, Button, Grid, HStack, Text } from "@chakra-ui/react";
+import { Divider, VStack } from "@chakra-ui/react";
 import SectionHeading from "./SectionHeading";
-import { useState } from "react";
+import NumOfPetSkills from "./NumOfPetSkills";
+import NumOfSkillSets from "./NumOfSkillSets";
 
 const PetSkill = () => {
-    const [selectedNumOfSkillsOpt, setSelectedNumOfSkillsOpt] = useState(0);
-
-    const onClickHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        const target = e.target as HTMLElement;
-
-        const numOfSkills = target.closest("li")?.getAttribute("data-index");
-        if (numOfSkills === null || numOfSkills === undefined)
-            return;
-        const idx = parseInt(numOfSkills, 10);
-        setSelectedNumOfSkillsOpt(idx);
-    }
 
     return (
-        <Box>
+        <VStack justifyContent="start" width="100%">
             <SectionHeading title={"Skill"} />
-            <HStack>
-                <Text fontWeight="bold">Number of slots</Text>
-                <HStack as="ul" listStyleType="none" onClick={(e) => onClickHandler(e)}>
-                    {Array(5).fill(null).map((_, idx) => (
-                        <Button
-                            as="li"
-                            // color={selectedNumOfSkillsOpt === idx ? "white" : "initial"}
-                            colorScheme={selectedNumOfSkillsOpt === idx ? "yellow" : "gray"}
-                            px={4} py={2} cursor="pointer" key={idx}
-                            data-index={idx}
-                        >
-                            <Text fontWeight="bold">{idx + 4}</Text>
-                        </Button>
-                    ))}
-                </HStack>
-            </HStack>
-            <Grid as="ul" listStyleType="none" templateColumns="repeat(4, 1fr)">
-
-            </Grid>
-        </Box>
+            <NumOfPetSkills />
+            <Divider margin={4} />
+            <NumOfSkillSets />
+        </VStack>
     )
 }
 

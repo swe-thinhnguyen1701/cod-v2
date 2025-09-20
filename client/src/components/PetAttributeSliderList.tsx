a28b4e4595d03733c34fd2ca81e12faa6dc446e3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-09-20 13:24:07 -0700

    feat: build PetAttributeSliderList component

---
+++ client/src/components/PetAttributeSliderList.tsx
@@ -0,0 +1,71 @@
+
+// width: 347 points = 200px
+
+import { Box, Grid, GridItem, HStack, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Text, VStack } from "@chakra-ui/react";
+import usePetStore from "../state-management/petStore";
+import { memo, useState } from "react";
+import type { PetAttribute } from "../entities/PetEntity";
+
+// i.e. 200 points = (200 * 200 / 347)px
+const DEFAULT_ATTRIBUTE_BAR_WIDTH = [100, 200];
+const MAX_ATTRIBUTE_VAL = 347;
+
+interface Props {
+    defaultAttributes: PetAttribute[];
+}
+
+const PetAttributeSliderList = memo(({ defaultAttributes }: Props) => {
+    const { setAttributeAt } = usePetStore();
+    const [sliderValue, setSliderValue] = useState<PetAttribute[]>(defaultAttributes);
+
+    const onChangeHandler = (idx: number, value: number) => {
+        setSliderValue(prev => {
+            const newSliderValue = [...prev];
+            newSliderValue[idx] = { ...newSliderValue[idx], value: value };
+            return newSliderValue;
+        });
+        setAttributeAt(idx, value);
+    }
+
+    return (
+        <Grid
+            as="ul"
+            templateColumns="repeat(2, 1fr)"
+            gridGap={{ base: 2, md: 4 }}
+            listStyleType="none"
+            pl={{ md: 4 }}
+        >
+            {defaultAttributes.map((att, idx) => (
+                <GridItem as="li" key={idx} minWidth={{ base: "150px", md: "250px" }}>
+                    <VStack alignItems="start" gap={0}>
+                        <Text fontWeight="bold">{att.name}</Text>
+                        <HStack width="100%" gap={{ base: 2, md: 4 }}>
+                            <Box width={{
+                                base: `${Math.round(DEFAULT_ATTRIBUTE_BAR_WIDTH[0] * att.value / MAX_ATTRIBUTE_VAL)}px`,
+                                sm: `${Math.round(DEFAULT_ATTRIBUTE_BAR_WIDTH[1] * att.value / MAX_ATTRIBUTE_VAL)}px`,
+                            }}
+                                transition="width 0.3s ease-in-out"
+                            >
+                                <Slider
+                                    aria-label="pet attribute slider"
+                                    defaultValue={att.value}
+                                    min={0}
+                                    max={att.value}
+                                    onChange={(val) => onChangeHandler(idx, val)}
+                                >
+                                    <SliderTrack>
+                                        <SliderFilledTrack />
+                                    </SliderTrack>
+                                    <SliderThumb />
+                                </Slider>
+                            </Box>
+                            <Text fontWeight="bold">{sliderValue[idx].value}</Text>
+                        </HStack>
+                    </VStack>
+                </GridItem>
+            ))}
+        </Grid>
+    )
+})
+
+export default PetAttributeSliderList;

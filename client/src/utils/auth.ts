63028f210ab220b7a43bc5093d27b1390071589c
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2025-09-29 06:29:41 -0700

    feat: build AuthService class

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,32 @@
+import {jwtDecode} from 'jwt-decode';
+
+class AuthService {
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    isTokenExpired(token: string) {
+        try {
+            const decoded: any = jwtDecode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.assign('/');
+    }
+}
+
+export default new AuthService();

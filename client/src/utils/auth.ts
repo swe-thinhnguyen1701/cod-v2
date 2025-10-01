f2f56aa3a7f3b2621123d2ceaf4c253d4452c5b3
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2025-10-01 00:23:59 -0700

    feat: add login method to store token in the local storage

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,4 +1,4 @@
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 class AuthService {
     loggedIn() {
@@ -21,6 +21,11 @@ class AuthService {
 
     getToken() {
         return localStorage.getItem('id_token');
+    }
+
+    login(token: string) {
+        localStorage.setItem('id_token', token);
+        window.location.assign('/');
     }
 
     logout() {

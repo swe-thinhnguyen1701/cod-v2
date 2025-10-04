1edbcebcf942bb0d7bacc6520be87b8f62937ac1
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2025-10-04 08:34:41 -0700

    feat: build UserAccordion component

---
+++ client/src/components/UserAccordion.tsx
@@ -0,0 +1,44 @@
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Avatar, HStack, ListItem, Text, UnorderedList } from "@chakra-ui/react"
+import { useAuthStore } from "../state-management/authStore";
+import { Link } from "react-router-dom";
+
+const UserAccordion = () => {
+    const { user, logout } = useAuthStore();
+
+    if (!user) return null;
+
+    return (
+        <Accordion allowToggle >
+            <AccordionItem border="none">
+                <AccordionButton padding={0} display="flex" justifyContent="space-between">
+                    <HStack>
+                        <Avatar size="sm" name={user.name} src={user.image ?? ""} />
+                        <Text as="span">{user.name}</Text>
+                    </HStack>
+                    <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                        <UnorderedList>
+                            <ListItem>
+                                <Link to="/profile">
+                                    Profile
+                                </Link>
+                            </ListItem>
+                            {user.isAdmin &&
+                                <ListItem>
+                                    <Link to="/admin">
+                                        Admin Management
+                                    </Link>
+                                </ListItem>
+                            }
+                            <ListItem onClick={logout}>
+                                <Text>Logout</Text>
+                            </ListItem>
+                        </UnorderedList>
+                    </AccordionPanel>
+            </AccordionItem>
+        </Accordion>
+    )
+}
+
+export default UserAccordion;

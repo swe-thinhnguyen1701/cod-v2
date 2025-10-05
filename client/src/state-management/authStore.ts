b60aa7dd500145c9039b189391818d67dcfd7807
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-10-05 07:23:47 -0700

    refactor: Fix login issue caused by Apollo cache reusing old data.

--- client/src/state-management/authStore.ts
+++ client/src/state-management/authStore.ts
@@ -5,46 +5,50 @@ import AuthService from "../utils/auth";
 import type User from "../entities/UserEntity";
 
 interface AuthState {
-    token: string | null;
-    user: User | null;
-    isLoggedIn: boolean;
-    login: (token: string) => Promise<void>;
-    logout: () => void;
-    checkAuth: () => Promise<void>;
+  token: string | null;
+  user: User | null;
+  isLoggedIn: boolean;
+  login: (token: string) => Promise<void>;
+  logout: () => Promise<void>;
+  checkAuth: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-    token: AuthService.getToken(),
-    user: null,
-    isLoggedIn: !!AuthService.getToken() && !AuthService.isTokenExpired(AuthService.getToken()!),
+  token: AuthService.getToken(),
+  user: null,
+  isLoggedIn:
+    !!AuthService.getToken() &&
+    !AuthService.isTokenExpired(AuthService.getToken()!),
 
-    login: async (token: string) => {
-        AuthService.saveToken(token);
-        // console.log(token);
-        try {
-            const { data } = await client.query({ query: GET_ME });
-            // console.log(data);
-            set({ token, user: data.me, isLoggedIn: true });
-        } catch (error) {
-            set({ token: null, user: null, isLoggedIn: false })
-        }
-    },
-    logout: () => {
-        AuthService.clearToken();
-        set({ token: null, user: null, isLoggedIn: false });
-    },
-    checkAuth: async () => {
-        const token = AuthService.getToken();
-        if (!token || AuthService.isTokenExpired(token)) {
-            set({ token: null, user: null, isLoggedIn: false });
-            return;
-        }
-
-        try {
-            const {data} = await client.query({query: GET_ME});
-            set({ token, isLoggedIn: true, user: data.me });
-        } catch {
-            set({ token: null, user: null, isLoggedIn: false });
-        }
+  login: async (token: string) => {
+    AuthService.saveToken(token);
+    try {
+      const { data } = await client.query({
+        query: GET_ME,
+        fetchPolicy: "network-only",
+      });
+      set({ token, user: data.me, isLoggedIn: true });
+    } catch {
+      set({ token: null, user: null, isLoggedIn: false });
     }
-}))
+  },
+  logout: async () => {
+    AuthService.clearToken();
+    await client.clearStore();
+    set({ token: null, user: null, isLoggedIn: false });
+  },
+  checkAuth: async () => {
+    const token = AuthService.getToken();
+    if (!token || AuthService.isTokenExpired(token)) {
+      set({ token: null, user: null, isLoggedIn: false });
+      return;
+    }
+
+    try {
+      const { data } = await client.query({ query: GET_ME });
+      set({ token, isLoggedIn: true, user: data.me });
+    } catch {
+      set({ token: null, user: null, isLoggedIn: false });
+    }
+  },
+}));

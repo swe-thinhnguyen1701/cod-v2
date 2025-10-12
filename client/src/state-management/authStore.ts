274ed61a17dbfe88112000135a16f51d313942c4
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-10-11 22:35:49 -0700

    feat: add loading property

--- client/src/state-management/authStore.ts
+++ client/src/state-management/authStore.ts
@@ -8,6 +8,7 @@ interface AuthState {
   token: string | null;
   user: User | null;
   isLoggedIn: boolean;
+  loading: boolean;
   login: (token: string) => Promise<void>;
   logout: () => Promise<void>;
   checkAuth: () => Promise<void>;
@@ -19,36 +20,43 @@ export const useAuthStore = create<AuthState>((set) => ({
   isLoggedIn:
     !!AuthService.getToken() &&
     !AuthService.isTokenExpired(AuthService.getToken()!),
+  loading: true,
 
   login: async (token: string) => {
+    set({ loading: true });
+
     AuthService.saveToken(token);
     try {
       const { data } = await client.query({
         query: GET_ME,
         fetchPolicy: "network-only",
       });
-      set({ token, user: data.me, isLoggedIn: true });
+      set({ token, user: data.me, isLoggedIn: true, loading: false });
     } catch {
-      set({ token: null, user: null, isLoggedIn: false });
+      set({ token: null, user: null, isLoggedIn: false, loading: false });
     }
   },
   logout: async () => {
+    set({ loading: true });
+
     AuthService.clearToken();
     await client.clearStore();
-    set({ token: null, user: null, isLoggedIn: false });
+    set({ token: null, user: null, isLoggedIn: false, loading: false });
   },
   checkAuth: async () => {
+    set({ loading: true });
+
     const token = AuthService.getToken();
     if (!token || AuthService.isTokenExpired(token)) {
-      set({ token: null, user: null, isLoggedIn: false });
+      set({ token: null, user: null, isLoggedIn: false, loading: false });
       return;
     }
 
     try {
       const { data } = await client.query({ query: GET_ME });
-      set({ token, isLoggedIn: true, user: data.me });
+      set({ token, isLoggedIn: true, user: data.me, loading: false });
     } catch {
-      set({ token: null, user: null, isLoggedIn: false });
+      set({ token: null, user: null, isLoggedIn: false, loading: false });
     }
   },
 }));

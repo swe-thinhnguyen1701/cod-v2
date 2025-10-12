61b1ffad09899238670d75f64c009e3dc434b3e3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-10-11 22:38:39 -0700

    feat: build CategorySidebar component

---
+++ client/src/components/admin/CategorySidebar.tsx
@@ -0,0 +1,43 @@
+import { Box, Heading, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import useCategoryStore from "../../state-management/categoryStore";
+import MENU_FEATURES from "../../config/menu-features";
+
+const CategorySidebar = () => {
+    const { selectedCategory, setSelectedCategory } = useCategoryStore();
+
+    return (
+        <Box>
+            {MENU_FEATURES.map((feature, featureIdx) => (
+                <Box key={featureIdx}>
+                    <Heading as="h4" fontSize="1.2rem" mb={2}>
+                        {feature.title}
+                    </Heading>
+                    <UnorderedList listStyleType="none" margin={0} paddingLeft={6}>
+                        {feature.categories.map((category, categoryIdx) => (
+                            <ListItem
+                                key={categoryIdx}
+                                onClick={() => setSelectedCategory(category)}
+                                cursor="pointer"
+                                value={category}
+                                backgroundColor={selectedCategory === category ? "#c3ab76" : "unset"}
+                                color={selectedCategory === category ? "white" : "inherit"}
+                                _hover={{ backgroundColor: "#c3ab76", color: "white" }}
+                                transitionProperty="background-color, color"
+                                transitionDuration="0.3s"
+                                transitionTimingFunction="ease-in-out"
+                                padding={2}
+                                rounded={4}
+                            >
+                                <Text textTransform="capitalize">
+                                    {category}
+                                </Text>
+                            </ListItem>
+                        ))}
+                    </UnorderedList>
+                </Box>
+            ))}
+        </Box>
+    )
+}
+
+export default CategorySidebar;
